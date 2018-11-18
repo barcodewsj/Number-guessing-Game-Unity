8edf71720a2502c4f5808cd6258de7a52d82450e
barcodewsj <38012195+barcodewsj@users.noreply.github.com>
2018-11-18 13:38:59 +0900

    Add files via upload

---
+++ SceneLoader.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+//This script should almost be universal, right?
+
+public class SceneLoader : MonoBehaviour {
+
+    public void LoadNextScene()
+    {
+        // Declared and defined an index for the current scene.
+
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+
+    public void LoadStartScrene()
+    {
+        SceneManager.LoadScene(0);  
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
